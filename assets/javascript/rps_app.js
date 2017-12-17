fb757bf880036e812cfbc13534d80f70172be0b7
Gerardo Fernandez <gerardojose13@gmail.com>
2017-12-17 00:57:30 -0800

    pushes updates

--- assets/javascript/rps_app.js
+++ assets/javascript/rps_app.js
@@ -17,6 +17,18 @@ var database = firebase.database()
 //---------------  THIS CODE WORKS   --------------------
 var name;
 var RPS;
+var player1;
+var player2;
+var choice1;
+var choice2;
+var player1Wins = 0;
+var player2Wins = 0;
+var player1Losses = 0;
+var player2Losses = 0;
+var ties = 0;
+var playerOnePick;
+var playerTwoPick;
+
 
 
 $('#name-submit').on('click', function(event){
@@ -50,10 +62,59 @@ $('.carousel').on('click', '.carousel-item', function(){
 	// var playerId = localStorage.getItem('playerId');
 	// console.log('this player just played', playerId);
 	RPS = $(this).attr('data-info');
+	playerOnePick = RPS;
+	playerTwoPick = RPS;
 
 	var choiceAdded = database.ref('choice').push({
 		choice: RPS
 	})
+
+	if(playerOnePick === playerTwoPick){
+		$('#result').html("<h3>It's a tie!</h3>")
+		ties++;
+
+		if (playerOnePick === 'rock' && playerTwoPick === 'scissors'){
+			$('#result').html("<h3>Player one wins!</h3>");
+			player1Wins++;
+			player2Losses++;
+			$('#player1-wins').html(player1Wins);
+			$('#player2-losses').html(player2Losses);
+		}
+		if (playerOnePick === 'paper' && playerTwoPick === 'rock'){
+			$('#result').html("<h3>Player one wins!</h3>");
+			player1Wins++
+			$('#player1-wins').html(player1Wins);	
+			$('#player2-losses').html(player2Losses);
+		}
+		if (playerOnePick === 'scissors' && playerTwoPick === 'paper'){
+			$('#result').html("<h3>Player one wins!</h3>");
+			player1Wins++
+			$('#player1-wins').html(player1Wins);	
+			$('#player2-losses').html(player2Losses);	
+		}
+		if (playerOnePick === 'rock' && playerTwoPick === 'paper'){
+			$('#result').html("<h3>Player Two wins!</h3>");
+			player2Wins++;
+			player1Losses++;
+			$('#player2-wins').html(player2Wins);
+			$('#player1-losses').html(player1Losses);
+		}
+		if (playerOnePick === 'paper' && playerTwoPick === 'scissors'){
+			$('#result').html("<h3>Player Two wins!</h3>");
+			player2Wins++;
+			player1Losses++;
+			$('#player2-wins').html(player2Wins);
+			$('#player1-losses').html(player1Losses);	
+		}
+		if (playerOnePick === 'scissors' && playerTwoPick === 'rock'){
+			$('#result').html("<h3>Player Two wins!</h3>");
+			player2Wins++;
+			player1Losses++;
+			$('#player2-wins').html(player2Wins);
+			$('#player1-losses').html(player1Losses);		
+		}
+
+	}
 });
 
 
